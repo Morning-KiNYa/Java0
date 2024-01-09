ceb1cb5a48db5b137f2aca1bcc59700ff81f9899
Morning-KiNYa <morning.master765@gmail.com>
2024-01-09 16:33:12 +0900

    初めてのコミット

---
+++ src/Main5.java
@@ -0,0 +1,13 @@
+
+public class Main5 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int tate = 3;
+		int yoko = 5;
+		int answer = tate*yoko;
+		System.out.println("縦幅"+tate+"横幅"+yoko+"の長方形の面積は、"+answer);
+
+	}
+
+}
